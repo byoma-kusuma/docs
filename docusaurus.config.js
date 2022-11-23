4d42adb4e091a77308806aeb7ba883c9d9d97c55
g-bibek <bibek@omnidek.com>
2022-11-22 19:03:43 -0500

    edit document url change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,13 +36,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/byoma-kusuma/docs/",
+          editUrl: "https://github.com/byoma-kusuma/docs/tree/dev",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/byoma-kusuma/docs/",
+          editUrl: "https://github.com/byoma-kusuma/docs/tree/dev",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
